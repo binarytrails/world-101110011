ae391dce258679ee9710c1afaaf7cbc855df2cf9
Seva Ivanov <seva@tumahn.net>
2017-03-31 17:56:36 -0400

    Main: add world dimensions as args

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,14 +4,25 @@
 */
 
 #include <stdio.h>
+#include <sstream>
 
 #include "World.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
-    const uint8_t w = 200;
-    const uint8_t h = 200;
+    if (argc < 3)
+    {
+        printf("You need to provide width & height.\n");
+        return 1;
+    }
 
+    unsigned int w;
+    unsigned int h;
+    std::stringstream w_str(argv[1]);
+    std::stringstream h_str(argv[2]);
+    w_str >> w; h_str >> h;
+
+    printf("Generating a world %ix%i..\n", w, h);
     World *world = new World(w, h);
 
     world->draw();
