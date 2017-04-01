bada50b86300cf07dd46db34f614d11a5873ab11
sami <sami@boubaker.me>
2017-04-01 14:23:38 -0400

    Added draw function to GLObject, draw function support instancing.

--- src/GLObject.h
+++ src/GLObject.h
@@ -1,9 +1,20 @@
 #ifndef GLOBJECT_H
 #define GLOBJECT_H
 
-#include <GLFW/glfw3.h>
-#include <vector>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
 
+// GLFW
+#include <GLFW/glfw3.h>
+
+// GLM Mathematics
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <complex.h>
+
+#include <vector>
 #include "Point.h"
 
 class GLObject {
@@ -15,6 +26,8 @@ public:
     int iSize;
     int iMax = 0;
     Point offset = Point(0,0,0);
+    GLuint VBO, instanceVBO, VAO, EBO;
+    int instanceCount = -1;
     
     GLObject() : vSize(0), iSize(0) {};
     GLObject(int vSize, int iSize) : vSize(vSize), iSize(iSize) {};
@@ -31,6 +44,7 @@ public:
     float sizeMax[3] = {0, 0, 0};
     
     GLObject add(GLObject* other);
+    void draw(glm::vec3* translations, int count);
     
     virtual ~GLObject();
 protected:
