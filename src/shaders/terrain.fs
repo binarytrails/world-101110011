768157d0e4291fbf87eece398811369343335243
zigman32 <zigman32@gmail.com>
2017-04-13 19:25:52 -0400

    Added illumination to terrain

--- src/shaders/terrain.fs
+++ src/shaders/terrain.fs
@@ -1,6 +1,7 @@
 /*
  * @file
  * @author Vsevolod (Seva) Ivanov
+ * Illumnation source: https://learnopengl.com/code_viewer.php?code=lighting/basic_lighting&type=fragment
 */
 
 #version 330 core
@@ -24,6 +25,30 @@ void main()
 */
 
     // TODO texture influenced by elevation
-
-    color = texture(soilTexture, texCoord);
+	vec4 textureColor = texture(soilTexture, texCoord);
+	
+	float ambientStrength = 0.1f;
+    vec3 ambient = ambientStrength * lightColor;
+  	
+    // Diffuse 
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - pos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+    
+    // Specular
+    float specularStrength = 0.5f;
+    vec3 viewDir = normalize(viewPos - pos);
+    vec3 reflectDir = reflect(-lightDir, norm);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;  
+        
+    vec4 result;
+	result.x = (ambient + diffuse + specular) * textureColor.x;
+	result.y = (ambient + diffuse + specular) * textureColor.y;
+	result.z = (ambient + diffuse + specular) * textureColor.z;
+	result.w = textureColor.w;
+	
+	
+    color = result;
 }
