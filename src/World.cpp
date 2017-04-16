1cc0751c22008ee986455d5f77155d8a9dc6ecc3
Seva Ivanov <seva@tumahn.net>
2017-04-16 12:13:39 -0400

    World: refactor mouse & keys callbacks

--- src/World.cpp
+++ src/World.cpp
@@ -189,7 +189,7 @@ void World::draw()
     while (!glfwWindowShouldClose(this->window->get()))
     {
         glfwPollEvents();
-        doMovement(this->window->get());
+        applyKeyboardBindings(this->window->get());
 
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
--- src/World.hpp
+++ src/World.hpp
@@ -117,24 +117,12 @@ static void framebufferSizeCallback(GLFWwindow* w, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-/*
-    Note: In order to ensure a smooth input handling from our application,
-    a boolean array is created to hold GLFW key values. If a key's particular
-    index is 0 (i.e. false), then it is not pressed or has been released by the
-    user. If a key's particular index is 1 (i.e true), then the key is pressed.
-
-    This array is then used in conjunction with function doMovement(), which
-    effectively carries out the neccesary camera system adjustments based on input.
-
-    This code was taken from www.learnopengl.com.
-
-    TODO: Possible adjustments to camera speed.
-*/
+//! Storing window pressed keys for faster processing
 static bool keys[256];
-static void doMovement(GLFWwindow* w);
+//! Applying window keys bindings
+static void applyKeyboardBindings(GLFWwindow* w);
 
-// END OF LEARNOPENGL CODE.
-
+//! Only registering as boolean if a key is pressed for faster processing
 static void keyCallback(GLFWwindow* w, int key, int scancode, int action, int mode)
 {
     if(action == GLFW_PRESS)
@@ -143,29 +131,28 @@ static void keyCallback(GLFWwindow* w, int key, int scancode, int action, int mo
         keys[key] = false;
 }
 
-static void doMovement(GLFWwindow* w)
+static void applyKeyboardBindings(GLFWwindow* w)
 {
     CallbackContext* cbcPtr = getWindowContext(w);
 
-    if (keys[GLFW_KEY_LEFT])   cbcPtr->world->rotate(glm::vec3(0, 1, 0));
-    if (keys[GLFW_KEY_RIGHT])  cbcPtr->world->rotate(glm::vec3(0, -1, 0));
-    if (keys[GLFW_KEY_UP])     cbcPtr->world->rotate(glm::vec3(1, 0, 0));
-    if (keys[GLFW_KEY_DOWN])   cbcPtr->world->rotate(glm::vec3(-1, 0, 0));
+    if (keys[GLFW_KEY_LEFT])    cbcPtr->world->rotate(glm::vec3(0, 1, 0));
+    if (keys[GLFW_KEY_RIGHT])   cbcPtr->world->rotate(glm::vec3(0, -1, 0));
+    if (keys[GLFW_KEY_UP])      cbcPtr->world->rotate(glm::vec3(1, 0, 0));
+    if (keys[GLFW_KEY_DOWN])    cbcPtr->world->rotate(glm::vec3(-1, 0, 0));
 
-    if (keys[GLFW_KEY_W]) cbcPtr->world->getCamera()->moveUp();
-    if (keys[GLFW_KEY_S]) cbcPtr->world->getCamera()->moveDown();
-    if (keys[GLFW_KEY_D]) cbcPtr->world->getCamera()->moveLeft();
-    if (keys[GLFW_KEY_A]) cbcPtr->world->getCamera()->moveRight();
+    if (keys[GLFW_KEY_W])       cbcPtr->world->getCamera()->moveUp();
+    if (keys[GLFW_KEY_S])       cbcPtr->world->getCamera()->moveDown();
+    if (keys[GLFW_KEY_D])       cbcPtr->world->getCamera()->moveLeft();
+    if (keys[GLFW_KEY_A])       cbcPtr->world->getCamera()->moveRight();
 
-    if (keys[GLFW_KEY_I]) cbcPtr->world->getCamera()->moveForward();
-    if (keys[GLFW_KEY_O]) cbcPtr->world->getCamera()->moveBackward();
+    if (keys[GLFW_KEY_I])       cbcPtr->world->getCamera()->moveForward();
+    if (keys[GLFW_KEY_O])       cbcPtr->world->getCamera()->moveBackward();
 
-    if (keys[GLFW_KEY_T]) cbcPtr->world->setRenderMode(GL_TRIANGLES);
-    if (keys[GLFW_KEY_L]) cbcPtr->world->setRenderMode(GL_LINES);
-    if (keys[GLFW_KEY_P]) cbcPtr->world->setRenderMode(GL_POINTS);
+    if (keys[GLFW_KEY_T])       cbcPtr->world->setRenderMode(GL_TRIANGLES);
+    if (keys[GLFW_KEY_L])       cbcPtr->world->setRenderMode(GL_LINES);
+    if (keys[GLFW_KEY_P])       cbcPtr->world->setRenderMode(GL_POINTS);
 
-    // Close window
-    if(keys[GLFW_KEY_ESCAPE]) glfwSetWindowShouldClose(w, GL_TRUE);
+    if(keys[GLFW_KEY_ESCAPE])   glfwSetWindowShouldClose(w, GL_TRUE);
 }
 
 static void mouseKeyCallback(GLFWwindow* w, int key, int action, int mode)
@@ -184,8 +171,7 @@ static void mouseKeyCallback(GLFWwindow* w, int key, int action, int mode)
     }
 }
 
-// TODO : Implement mouse control.
-
+// FIXME #2 Mouse camera resets to default angle after each new look
 static void mousePositionCallback(GLFWwindow* w, double xpos, double ypos)
 {
     CallbackContext* cbcPtr = getWindowContext(w);
@@ -193,60 +179,58 @@ static void mousePositionCallback(GLFWwindow* w, double xpos, double ypos)
     if (cbcPtr->world->mousePressed)
     {
         // Delta of position along x-axis.
-        GLfloat xoffset = xpos - cbcPtr->world->lastX;
+        GLfloat xoff = xpos - cbcPtr->world->lastX;
 
         // Delta of positon along y-axis.
-        GLfloat yoffset = ypos - cbcPtr->world->lastY;
+        GLfloat yoff = ypos - cbcPtr->world->lastY;
 
         // Reset our x and y positions for the frame.
         cbcPtr->world->lastX = xpos;
         cbcPtr->world->lastY = ypos;
 
-        // In order to lessen the jerk of mouse movement, we add this sensitivity to offsets.
+        // In order to lessen the jerk of mouse movement,
+        // we add this sensitivity to offsets.
         GLfloat sensitivity = 0.05f;
-        xoffset *= sensitivity;
-        yoffset *= sensitivity;
+        xoff *= sensitivity;
+        yoff *= sensitivity;
 
-        cbcPtr->world->yaw += xoffset;
-        cbcPtr->world->pitch += yoffset;
+        cbcPtr->world->yaw += xoff;
+        cbcPtr->world->pitch += yoff;
 
-        // Make sure that pitch is in the domain [-89.0, 89.0] (not exactly 90 due to cosine).
+        // Make sure that pitch is in the domain [-89.0, 89.0]
+        // (not exactly 90 due to cosine).
         if(cbcPtr->world->pitch > 89.0f)
             cbcPtr->world->pitch = 89.0f;
         if(cbcPtr->world->pitch < -89.0f)
             cbcPtr->world->pitch = -89.0f;
 
-        GLfloat final_pitch = cbcPtr->world->pitch;
-        GLfloat final_yaw = cbcPtr->world->yaw;
+        glm::vec3 at;
+        GLfloat pitch = cbcPtr->world->pitch;
+        GLfloat yaw = cbcPtr->world->yaw;
 
-        glm::vec3 viewDirection;
+        at.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+        at.y = sin(glm::radians(pitch));
+        at.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
 
-        viewDirection.x = cos(glm::radians(final_pitch)) * cos(glm::radians(final_yaw));
-        viewDirection.y = sin(glm::radians(final_pitch));
-        viewDirection.z = cos(glm::radians(final_pitch)) * sin(glm::radians(final_yaw));
-
-        glm::vec3 normalized =  glm::normalize(viewDirection);
-
-        cbcPtr->world->getCamera()->setAt(normalized);
+        cbcPtr->world->getCamera()->setAt(glm::normalize(at));
     }
 }
 
-static void mouseScrollCallback(GLFWwindow *w, double xoffset, double yoffset)
+static void mouseScrollCallback(GLFWwindow *w, double xoff, double yoff)
 {
     CallbackContext* cbcPtr = getWindowContext(w);
     /* TODO
-     *  avance:
+     *  Terrain::advance
      *      1. detect direction
      *      2. if going on positive x or z axis then
      *              advance terrain in this direction
      */
-
-    if (yoffset > 0)
+    if (yoff > 0)
     {
         //cbcPtr->world->getTerrain()->advance(true);
         cbcPtr->world->getCamera()->moveForward();
     }
-    else if (yoffset < 0)
+    else if (yoff < 0)
     {
         //cbcPtr->world->getTerrain()->advance(false);
         cbcPtr->world->getCamera()->moveBackward();
